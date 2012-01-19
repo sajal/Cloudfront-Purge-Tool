aa6932a917bb5f0ea181f02c8f2bda53e426575f
Sajal Kayan <sajal83@gmail.com>
2012-01-19 21:12:38 +0700

    first commit

---
+++ cloudfrontapi.js
@@ -0,0 +1,165 @@
+/*
+  Author: Sajal Kayan
+  URL: http://www.cdnplanet.com/
+  License: Undecided probably BSD style
+  Description:  Access to CloudFront API from browser. Requires crossdomain XHR!!!.
+                Wont work from webpages, but i'm making this for chrome extention.
+  Sample Usage : 
+    var c = new cloudfrontapi("XXXXXXXXXX", "XXXXXXXXX");
+    c.usesystemtime = false; // set this if users system clock is innacurate by more than 15 mins
+    var print = function(obj){
+      console.log(obj);
+    }
+    c.getAllDistributions(print);
+    c.getAllInvalidations("DISTIDXXX", print);
+    c.getPendingInvalidations("DISTIDXXX", print);
+  
+  Dependencies: http://davidwalsh.name/convert-xml-json , http://www.java2s.com/Open-Source/Javascript/Benchmark/sunspider-mod/tests/sunspider-0.9.1/crypto-sha1.js.htm
+
+*/
+
+
+var cloudfrontapi = function(aws_access, aws_secret){
+  this.aws_access = aws_access;
+  this.aws_secret = aws_secret;
+  this.usesystemtime = true;
+  this.endpoint = "cloudfront.amazonaws.com";
+//  this.tmpdatestr = ""
+
+  this.getAWStime = function(){
+    var req = new XMLHttpRequest();
+    req.open(
+      "GET",
+      "https://cloudfront.amazonaws.com/date",
+      false
+    );
+    req.send(null);
+    return req.getResponseHeader("Date");
+  }
+
+  this.getDate = function(){
+    if (this.usesystemtime){
+      return (new Date()).toGMTString();
+    } else {
+      //fetch the date from AWS, since we dont trust system time
+      return this.getAWStime()
+    }
+  }
+
+  this.getAuth = function(){
+    var datestr = this.getDate();
+    var signature = sha1.hmac.toB64(this.aws_secret, datestr);
+    var authorization = "AWS " + this.aws_access + ":" + signature + "="; // no idea why i gotta manually add = but this shit works
+    return {datestr: datestr, authorization:authorization}
+  };
+
+  this.makeRequest = function(path, callback, data){
+    var req = new XMLHttpRequest();
+    var auth = this.getAuth();
+    var method = "POST";
+    if (data == undefined){
+      method = "GET";
+      data == null;
+    }
+    req.open(
+      method,
+      "https://" + this.endpoint + path,
+      true
+    );
+    req.setRequestHeader("Authorization", auth.authorization);
+    req.setRequestHeader("x-amz-date", auth.datestr);
+    if (method == "POST"){
+      req.setRequestHeader("Content-Type", "text/xml");
+    }
+    req.onload = function(){
+      callback(req);
+    };
+    req.send(data);
+  }
+
+  this.getfield = function(node, tagname){
+    console.log(tagname);
+    console.log(node);
+    return node.getElementsByTagName(tagname)[0].textContent
+  }
+
+  this.getAllDistributions = function(callback){
+    /*
+    Returns Array : List of CloudFront distributions.
+    */
+    var getfield = this.getfield;
+    var localcb = function(res){
+      var distributions = xmlToJson(res.responseXML).DistributionList.DistributionSummary;
+      if (distributions.length == undefined){
+        distributions = [distributions];
+      }
+      callback(distributions);
+    };
+    this.makeRequest("/2010-11-01/distribution", function(res){ localcb(res) })
+  };
+
+  this.getAllInvalidations = function(distid, callback){
+    /*
+    Returns Array : List of last 20 invalidation requests.
+    */
+    var getfield = this.getfield;
+    var localcb = function(res){
+      var invalidations = xmlToJson(res.responseXML).InvalidationList.InvalidationSummary;
+      if (invalidations == undefined){
+        callback([]);
+      } else {
+        if (invalidations.length == undefined){
+          invalidations = [invalidations];
+        }
+        callback(invalidations);
+      }
+    }
+    this.makeRequest("/2010-11-01/distribution/" + distid + "/invalidation?MaxItems=20", localcb);
+  };
+
+  this.getPendingInvalidations = function(distid, callback){
+    /*
+    Returns Array : List of last 20 invalidation requests (only filtering non completed ones).
+    */
+    var localcb = function(allinvalidations){
+      pending = [];
+      for(i=0;i<allinvalidations.length;i++){
+        if(allinvalidations[i].Status["#text"] != 'Completed'){
+          pending.push(allinvalidations[i]);
+        }
+      }
+      callback(pending);
+    }
+    this.getAllInvalidations(distid, localcb);
+  };
+
+  this.AddNewPurgeRequest = function(distid, pathcsv, callback){
+    /*
+    Places a purge request for comma seperated list of paths.
+    */
+    var localcb = function(res){
+      callback(res);
+    }
+    var files = pathcsv.split(',');
+    var invallist = "";
+    for(i=0;i<files.length;i++){
+      invallist += '<Path>' + files[i].trim() + '</Path>'
+    }
+    var callerref = "batch" + (new Date()).getTime();
+    var teststr = "<InvalidationBatch>" + invallist + "<CallerReference>" + callerref + "</CallerReference></InvalidationBatch>";
+    console.log(teststr);
+    var path = "/2010-11-01/distribution/" + distid + "/invalidation";
+    this.makeRequest(path, localcb, teststr);
+  };
+
+  this.getInvalidationDetails = function(distid, invalid, callback){
+    //todo
+    var localcb = function(res){
+      callback(xmlToJson(res.responseXML));
+    }
+    var path = "/2010-11-01/distribution/" + distid + "/invalidation/" + invalid;
+    console.log(path)
+    this.makeRequest(path, localcb);
+  };
+
+}
---
+++ script.js
@@ -0,0 +1,170 @@
+var distributionclickhandler = function(distid){
+  if (typeof(distid) == "string"){
+    var id = distid;
+  } else {
+    var id = this.getElementsByClassName("id")[0].innerHTML;
+  }
+  console.log(id);
+  window.currentdist = id;
+  cfobj.getAllInvalidations(id, updateinvalidations);
+}
+
+var invaldetails = function(){
+  var id = this.getElementsByClassName("id")[0].innerHTML;
+  $( "#invaldetails" )[0].innerHTML = "Loading..."
+  cfobj.getInvalidationDetails(currentdist, id);
+}
+
+var placepurgereq = function(box){
+  var flist = document.getElementById("csvpathlist").value;
+  console.log(flist);
+  console.log(currentdist);
+  cfobj.AddNewPurgeRequest(currentdist, flist, function(res){
+    console.log(res);
+    distributionclickhandler(currentdist);
+  });
+}
+
+var updateinvaldetails = function(obj){
+  //invaldetails
+  console.log(obj);
+  var id = obj.Invalidation.Id["#text"];
+  var created = obj.Invalidation.CreateTime["#text"];
+  var status = obj.Invalidation.Status["#text"];
+  var rawpaths = obj.Invalidation.InvalidationBatch.Path;
+  var paths = "";
+  if (rawpaths.length == undefined){
+    paths = rawpaths["#text"];
+  } else {
+    var patharr = [];
+    for (i=0;i<rawpaths.length;i++){
+      patharr.push(rawpaths[i]["#text"])
+    }
+    paths = patharr.join(",")
+  }
+  
+  var inval = $("#invaldetails")[0];
+  inval.innerHTML = '<h3 class="ui-widget-header ui-corner-all">Purge Details</h3>Id: ' + id + "<br>Created : " + created + "<br>Files: " + paths + " <br>Status: " + status
+  $( "#invaldetails" ).effect( "slide", {}, 500 );
+}
+
+
+var updatedistlist = function(dist){
+  console.log(dist);
+  var distributions = document.getElementById("distributions");
+  var ol = document.createElement('ul');
+  ol.id = "distlist";
+  for (i=0;i<dist.length;i++){
+    var box = document.createElement('li');
+    box.className = "distbox";
+    box.classList.add("ui-widget-content");
+    var el = document.createElement('div');
+    el.className = "id";
+    el.appendChild(document.createTextNode(dist[i].Id["#text"]));
+    var cname = document.createElement('div');
+    cname.className = "cnames";
+
+    var cn = document.createElement('div');
+    cn.className = "cname";
+    cn.appendChild(document.createTextNode(dist[i].DomainName["#text"]));
+    cname.appendChild(cn);        
+
+    var cnames = dist[i].CNAME;
+    if (cnames != undefined){
+      if (cnames["#text"] != undefined){
+        var cn = document.createElement('div');
+        cn.className = "cname";
+        cn.appendChild(document.createTextNode(cnames["#text"]));
+        cname.appendChild(cn);        
+      } else {
+        for(j=0;j<cnames.length;j++){
+          var cn = document.createElement('div');
+          cn.className = "cname";
+          cn.appendChild(document.createTextNode(cnames[j]["#text"]));
+          cname.appendChild(cn);
+        }        
+      }
+    }
+    console.log(cname);
+    box.appendChild(el);
+    box.appendChild(cname);
+//    box.onclick = distributionclickhandler;
+    ol.appendChild(box);
+    distributions.appendChild(ol);
+  }
+  distributions.style.display="";
+  $( "#distlist" ).selectable();
+  $( "#distlist" ).bind( "selectableselected", function(event, ui) {
+    id = ui.selected.getElementsByClassName("id")[0].innerHTML;
+    window.currentdist = id;
+    cfobj.getAllInvalidations(id, updateinvalidations);
+  });
+
+}
+
+var updateinvalidations = function(invals){
+  console.log(invals);
+  var invalidations = document.getElementById("invalidations");
+  //remove all childrens if exists
+  if ( invalidations.hasChildNodes() ){
+    while ( invalidations.childNodes.length >= 1 ){
+      invalidations.removeChild( invalidations.firstChild );       
+    } 
+  }
+  //<h3 class="ui-widget-header ui-corner-all">Invalidations</h3>
+  title = document.createElement('h3');
+  title.className = "ui-widget-header ui-corner-all";
+  title.appendChild(document.createTextNode("Invalidations"));
+  invalidations.appendChild(title);
+  var ol = document.createElement('ul');
+  ol.id = "invallist";
+  for (i=0;i<invals.length;i++){
+    var box = document.createElement('div');
+    box.className = "invalbox";
+    var el = document.createElement('div');
+    el.className = "id";
+    el.appendChild(document.createTextNode(invals[i].Id["#text"]));
+    var st = document.createElement('div');
+    st.className = invals[i].Status["#text"];
+    //box.classList.add(invals[i].Status["#text"]);
+    st.appendChild(document.createTextNode(invals[i].Status["#text"]));
+    box.appendChild(el);
+    box.appendChild(st);
+    //box.onclick = invaldetails;
+    ol.appendChild(box);
+    invalidations.appendChild(ol);
+  }
+  invalidations.style.display = "";
+  document.getElementById("newinval").style.display = "";
+  $( "#invallist" ).selectable();
+  $( "#invallist" ).bind( "selectableselected", function(event, ui) {
+    id = ui.selected.getElementsByClassName("id")[0].innerHTML;
+    //console.log(id);
+    cfobj.getInvalidationDetails(currentdist, id, updateinvaldetails);
+    //window.currentdist = id;
+    //cfobj.getAllInvalidations(id, updateinvalidations);
+  });
+}
+
+
+var auth = function(){
+  var secret = document.getElementById("AWSsecret").value
+  var access = document.getElementById("AWSaccess").value
+  window.cfobj = new cloudfrontapi(access, secret);
+  localStorage.access=access;
+  localStorage.secret=secret;
+  //hide the login box
+  var loginbox = document.getElementById("auth")
+  loginbox.style.display="none";
+  cfobj.getAllDistributions(updatedistlist);
+}
+
+
+$(document).ready(function() {
+  var access = localStorage.access;
+  var secret = localStorage.secret;
+  if ((access != undefined) && (secret != undefined)){
+    document.getElementById("AWSsecret").value = secret;
+    document.getElementById("AWSaccess").value = access;
+  }
+});
